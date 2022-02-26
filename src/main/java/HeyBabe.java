7e1d9887a6f3c6780129e7627989cf2d98567cf7
nazarii_tkachuk <nazarii.tkachuk@sombrainc.com>
2022-02-26 19:47:49 +0200

    third commit

--- src/main/java/HeyBabe.java
+++ src/main/java/HeyBabe.java
@@ -9,7 +9,7 @@ public class HeyBabe implements Runnable {
     }
 
     private static void extracted() {
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 1000; i++) {
             final HeyBabe heyBabe = new HeyBabe();
             final Thread thread = new Thread(heyBabe);
 
